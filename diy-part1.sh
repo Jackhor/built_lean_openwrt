ab95a7b478d659a2a643ae74c20c3763c81a5396
jjyy <jackhor@qq.com>
2021-01-16 09:28:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#rm -rf luci-theme-argon
-#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+#Change luci-theme-argon to latest
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
